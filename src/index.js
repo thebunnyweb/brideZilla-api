3f578f36c31e07995bbfc14ca9eb1840344f9c75
bunny <bunny@bunnys-MacBook-Pro.local>
2018-09-28 00:13:46 +0530

    Removed Docs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import polyfill from 'babel-polyfill'; /*eslint-disable-line*/
 import express from 'express';
-import ApiDoc from 'express-mongoose-docs';
-import mongoose from 'mongoose';
+// import ApiDoc from 'express-mongoose-docs';
+// import mongoose from 'mongoose';
 import db from './config/db'; /*eslint-disable-line*/
 import constants from './config/constants';
 import middlewareConfig from './config/middleware';
@@ -12,7 +12,7 @@ const PORT = constants.port;
 
 middlewareConfig(app);
 RouteConfig(app);
-ApiDoc(app, mongoose);
+// ApiDoc(app, mongoose);
 
 app.listen(PORT, err => {
   if (!err) console.log(`App running on port ${PORT}`);
