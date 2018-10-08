21129fc283fd838ed8dabab8236e265f888296b5
bunny <bunny@bunnys-MacBook-Pro.local>
2018-10-08 14:03:54 +0530

    Added cors

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const isDev = process.env.NODE_ENV === 'developement';
 
 export default app => {
   app.use(bodyParser.json());
+  app.use(cors());
   app.use(bodyParser.urlencoded({ extended: true }));
   if (isDev) {
     app.use(morgan('dev'));
