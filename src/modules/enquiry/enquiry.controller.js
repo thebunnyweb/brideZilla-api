c5d22daa63400e3db823ba4348b6eba4b32741b2
bunny <bunny@bunnys-MacBook-Pro.local>
2018-10-13 12:49:14 +0530

    Bug Fixes

--- src/modules/enquiry/enquiry.controller.js
+++ src/modules/enquiry/enquiry.controller.js
@@ -4,17 +4,17 @@ import EnquiriesModel from './enquiry.model';
 export async function addEnquiry(req, res) {
   try {
     const enquiry = await EnquiriesModel.create(req.body);
-    res.send(HttpStatus.CREATED).json(enquiry);
+    res.status(HttpStatus.CREATED).json(enquiry);
   } catch (error) {
-    res.send(HttpStatus.BAD_GATEWAY).json(error);
+    res.status(HttpStatus.BAD_GATEWAY).json(error);
   }
 }
 
 export async function getEnquiry(req, res) {
   try {
     const enquiry = await EnquiriesModel.find({});
-    res.send(HttpStatus.CREATED).json(enquiry);
+    res.status(HttpStatus.CREATED).json(enquiry);
   } catch (error) {
-    res.send(HttpStatus.BAD_GATEWAY).json(error);
+    res.status(HttpStatus.BAD_GATEWAY).json(error);
   }
 }
