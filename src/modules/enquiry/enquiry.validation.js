e4785bfc6818a8f5b7863aa144b21c44bac54a45
bunny <bunny@bunnys-MacBook-Pro.local>
2018-10-13 12:46:37 +0530

    Issue Fixes

--- src/modules/enquiry/enquiry.validation.js
+++ src/modules/enquiry/enquiry.validation.js
@@ -2,8 +2,10 @@ import Joi from 'joi';
 
 export default {
   EnquiryValidation: {
-    name: Joi.required(),
-    email: Joi.required().email(),
-    message: Joi.required()
+    name: Joi.string().required(),
+    email: Joi.string()
+      .required()
+      .email(),
+    message: Joi.string().required()
   }
 };
